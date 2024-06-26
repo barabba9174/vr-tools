a7c6a2c18ebf77c4d0233049228d26ad766915e4
BARBARA SCHIAVINATO <barbara.schiavinato@gmail.com>
2024-06-26 08:49:33 +0100

    feat:add deploy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // basePath: '/vr-italia/vr-tool',
-}
+  basePath: "/vr-tools",
+  output: "export",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
